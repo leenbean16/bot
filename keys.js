df2d44262104e6bca7399d862055b69a05296e95
leenbean16 <shamoonlina@gmail.com>
2017-06-03 19:57:35 -0400

    git ignore

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('ths is loaded');
+
+export.twitterKeys = {
+	consumer_key: 'dSBFxj9FxyArEflNarxfbzIcm'
+	consumer_secret: '	asrlb3SIPiee048yy2LuZIAsVw9Dg4kk9iYoae1jnbAmZD1GoP'
+	acces_token_key: '	862318244769574913-rL9dTQl63mJ5FNOqLRz7BWZcXjAYlYu'
+	access_token_secret: 'WP1d9mglPYrD4Q7EVyabF41O31oIoVnAakRDimVr5LogT'
+}
